f4a8a62dace8bc36c22d6fa6da815c3f4ed12eea
bricenf <bricen2002@gmail.com>
2021-03-18 11:50:29 -0400

    Updated Case 3

---
+++ Case 3/optimization.py
@@ -0,0 +1,12 @@
+#Goals
+#1. low correlations
+#   - Diversification
+#   - Low risk
+
+#2. high returns
+
+import pandas as pd
+
+Dataset1 = pd.read_csv("Case 3\Case3HistoricalPrices.csv")
+
+print(Dataset1)
